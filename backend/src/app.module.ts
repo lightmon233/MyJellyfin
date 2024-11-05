539577a6de2e4d71c6037f442bd954d16e52a832
lightmon <lightmon5210@outlook.com>
2024-11-06 01:00:47 +0800

    movie template

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { MovieModule } from './movie/movie.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'username',
+      password: 'password',
+      database: 'movie_db',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true, // 仅开发使用
+    }),
+    MovieModule,
+  ],
 })
 export class AppModule {}
