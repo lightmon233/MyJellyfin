61cffdd7a1e596a3064f19b64ca0e51b5f48c004
lightmon <lightmon5210@outlook.com>
2024-11-06 15:56:09 +0800

    update entity info

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -6,10 +6,10 @@ import { MovieModule } from './movie/movie.module';
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'username',
-      password: 'password',
+      host: '206.237.19.214',
+      port: 2289,
+      username: 'postgres',
+      password: 'postgres',
       database: 'movie_db',
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true, // 仅开发使用
