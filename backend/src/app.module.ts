5508554e10cdf34fc72fb12bbe4cc2e4d83914e6
lightmon <lightmon5210@outlook.com>
2024-11-06 23:09:33 +0800

    update server address

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -6,8 +6,8 @@ import { MovieModule } from './movie/movie.module';
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: '206.237.19.214',
-      port: 2289,
+      host: '43.143.98.164',
+      port: 5432,
       username: 'postgres',
       password: 'postgres',
       database: 'movie_db',
