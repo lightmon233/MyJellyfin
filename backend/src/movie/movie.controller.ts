fdf9d71a0edc294d1dd5212a08ec270b6f349586
lightmon <lightmon5210@outlook.com>
2024-11-09 17:08:07 +0800

    ok to download images to localPath

--- backend/src/movie/movie.controller.ts
+++ backend/src/movie/movie.controller.ts
@@ -6,10 +6,12 @@ export class MovieController {
   constructor(private readonly movieService: MovieService) {}
 
   @Get('scrape')
-  async scrapeMovie(@Query('names') names: string[]): Promise<string> {
-    for (const name of names) {
+  async scrapeMovie(@Query('names') names: string): Promise<string> {
+    const nameArray = names.split(',');
+    for (const name of nameArray) {
       await this.movieService.scrapeMovieData(name);
+      await new Promise(resolve => setTimeout(resolve, 1000));
     }
-    return `Movie data for [${names.join(', ')}] has been scraped and stored.`;
+    return `Movie data for [${nameArray.join(', ')}] has been scraped and stored.`;
   }
 }
