f2a0ed5ff101d92171a3307bbf33055e110a3038
lightmon <lightmon5210@outlook.com>
2024-11-09 16:10:58 +0800

    frontend env

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,18 +1,26 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { MovieModule } from './movie/movie.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: '43.143.98.164',
-      port: 5432,
-      username: 'postgres',
-      password: 'postgres',
-      database: 'movie_db',
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      synchronize: true, // 仅开发使用
+    ConfigModule.forRoot({
+      isGlobal: true, // 使读取环境变量的配置模块在整个应用中可用
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_NAME'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: true, // 仅开发使用
+      }),
+      inject: [ConfigService],
     }),
     MovieModule,
   ],
